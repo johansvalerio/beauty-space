713db3c2cf6b2a76f9911a8d90346a014ccad542
Susanogetsu <johans.valerio@hotmail.com>
2024-09-10 01:34:20 -0600

    Update footer colors and links to follow

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,34 @@
 import { Separator } from "@/components/ui/separator";
-import { Instagram, MessageCircle } from 'lucide-react';
+import { Instagram, MessageCircle, MapPinIcon, Mail } from 'lucide-react';
 
 export default function Footer() {
     return (
         <footer className="bg-white">
             <div className="container mx-auto px-4 py-8">
-                <div className="grid md:grid-cols-2 gap-8 p-4">
+                <div className="grid md:grid-cols-3 gap-8 p-4">
                     <div>
                         <h4 className="text-lg font-semibold mb-4">Contacto</h4>
                         <div className="flex flex-col gap-4">
-                            <p className="text-gray-600">Cañas, Gte.</p>
+
                             <div className="flex">
-                                <a href="https://wa.me/50688015998" target="_blank" className="flex gap-1 text-green-600 hover:text-green-400 font-semibold">WhatsApp<MessageCircle /></a>
+                                <a href="mailto:isabelbe9266@gmail.com" className="text-gray-600 flex gap-1 items-center hover:text-gray-400 font-semibold"> <Mail /> Email</a>
                             </div>
-                            <p className="text-gray-600">Email: isabelbe9266@gmail.com</p>
+                            <div className="flex">
+                                <a href="https://wa.me/50688015998" target="_blank" className="flex gap-1 items-center text-green-600 hover:text-green-400 font-semibold"><MessageCircle /> WhatsApp</a>
+                            </div>
                         </div>
                     </div>
                     <div>
                         <h4 className="text-lg font-semibold mb-4">Sígueme en</h4>
                         <div className="flex  gap-4">
                             {/* <a href="#" className="text-gray-600 flex gap-1 items-center hover:text-pink-600">Facebook <Facebook /> </a> */}
-                            <a href="https://www.instagram.com/beauty_space_cr/?hl=es" target="_blank" className="flex gap-1 items-center font-semibold text-pink-600 hover:text-pink-400">Instagram <Instagram /> </a>
+                            <a href="https://www.instagram.com/beauty_space_cr/?hl=es" target="_blank" className="flex gap-1 items-center font-semibold text-pink-600 hover:text-pink-400"><Instagram />Instagram  </a>
+                        </div>
+                    </div>
+                    <div>
+                        <h4 className="text-lg font-semibold mb-4">Ubicación</h4>
+                        <div className="flex  gap-4">
+                            <a href="https://maps.app.goo.gl/ev1UPxbUMjnhVSEX8" target="_blank" className="flex gap-1 items-center font-semibold text-blue-600 hover:text-blue-400"><MapPinIcon />Provincia de Guanacaste, Cañas, Bello Horizonte</a>
                         </div>
                     </div>
                 </div>
