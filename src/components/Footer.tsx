1dcc3ed8c0aec8380b22ae51c31f7e7b9ce79752
Susanogetsu <johans.valerio@hotmail.com>
2024-09-11 20:10:03 -0600

    Add underline to footer elements

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,28 +7,28 @@ export default function Footer() {
             <div className="container mx-auto px-4 py-8">
                 <div className="grid md:grid-cols-3 gap-8 p-4">
                     <div>
-                        <h4 className="text-lg font-semibold mb-4">Contacto</h4>
+                        <h4 className="text-lg font-semibold mb-4 text-black">Contacto</h4>
                         <div className="flex flex-col gap-4">
 
                             <div className="flex">
-                                <a href="mailto:isabelbe9266@gmail.com" className="text-gray-600 flex gap-1 items-center hover:text-gray-400 font-semibold"> <Mail /> Email</a>
+                                <a href="mailto:isabelbe9266@gmail.com" className="text-gray-600 flex gap-1 items-center hover:text-gray-400 font-semibold underline underline-offset-2"> <Mail /> Email</a>
                             </div>
                             <div className="flex">
-                                <a href="https://wa.me/50688015998" target="_blank" className="flex gap-1 items-center text-green-600 hover:text-green-400 font-semibold"><MessageCircle /> WhatsApp</a>
+                                <a href="https://wa.me/50688015998" target="_blank" className="flex gap-1 items-center text-green-600 hover:text-green-400 font-semibold underline underline-offset-2"><MessageCircle /> WhatsApp</a>
                             </div>
                         </div>
                     </div>
                     <div>
-                        <h4 className="text-lg font-semibold mb-4">Sígueme en</h4>
+                        <h4 className="text-lg font-semibold mb-4 text-black">Sígueme en</h4>
                         <div className="flex  gap-4">
                             {/* <a href="#" className="text-gray-600 flex gap-1 items-center hover:text-pink-600">Facebook <Facebook /> </a> */}
-                            <a href="https://www.instagram.com/beauty_space_cr/?hl=es" target="_blank" className="flex gap-1 items-center font-semibold text-pink-600 hover:text-pink-400"><Instagram />Instagram  </a>
+                            <a href="https://www.instagram.com/beauty_space_cr/?hl=es" target="_blank" className="flex gap-1 items-center font-semibold text-pink-600 hover:text-pink-400 underline underline-offset-2"><Instagram />Instagram  </a>
                         </div>
                     </div>
                     <div>
-                        <h4 className="text-lg font-semibold mb-4">Ubicación</h4>
+                        <h4 className="text-lg font-semibold mb-4 text-black ">Ubicación GPS</h4>
                         <div className="flex  gap-4">
-                            <a href="https://maps.app.goo.gl/ev1UPxbUMjnhVSEX8" target="_blank" className="flex gap-1 items-center font-semibold text-blue-600 hover:text-blue-400"><MapPinIcon />Provincia de Guanacaste, Cañas, Bello Horizonte</a>
+                            <a href="https://maps.app.goo.gl/ev1UPxbUMjnhVSEX8" target="_blank" className="flex gap-1 items-center font-semibold text-blue-600 hover:text-blue-400  underline underline-offset-2 border-blue-600"><MapPinIcon />Guanacaste, Cañas, Bello Horizonte</a>
                         </div>
                     </div>
                 </div>
